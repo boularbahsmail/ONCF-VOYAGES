45809f1ea9dc6040920449a7aeeb05dc5340bf43
Ismail Boularbah <qiranorane55@gmail.com>
2022-12-10 01:22:22 +0100

    Undo translating functionality #2

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
-import { useState } from "react";
-import styles from "./App.module.css";
 import { StatusBar, View } from "react-native";
+import styles from "./App.module.css";
 
 import Header from "./components/Header";
 import WelcomingCard from "./components/WelcomingCard";
 import SearchHistory from "./components/SearchHistory";
 
 export default function App() {
-  const [language, setLanguage] = useState("EN");
-
   return (
     <View style={styles.container}>
       <StatusBar />
-      <Header language={language} setLanguage={setLanguage} />
+      <Header />
       <WelcomingCard language={language} />
       <SearchHistory />
     </View>
--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -4,12 +4,7 @@ import styles from "../../styles/Header.module.css";
 import user_avatar from "../../assets/images/user_avatar.jpg";
 import app_logo from "../../assets/images/al-boraq-logo.png";
 
-const Header = ({ language, setLanguage }) => {
-  const switchLanguage = () => {
-    if (language != "FR") setLanguage("FR");
-    else setLanguage("EN");
-  };
-
+const Header = () => {
   return (
     <View style={styles.header}>
       <View>
@@ -23,13 +18,8 @@ const Header = ({ language, setLanguage }) => {
         <Image style={specialStyle.appLogo} source={app_logo} alt="App-Logo" />
       </View>
       <View>
-        <Pressable
-          style={styles.switchLanguageButton}
-          onPress={() => {
-            switchLanguage();
-          }}
-        >
-          <Text style={styles.switchLanguageButtonText}>{language}</Text>
+        <Pressable style={styles.switchLanguageButton}>
+          <Text style={styles.switchLanguageButtonText}>EN</Text>
         </Pressable>
       </View>
     </View>
